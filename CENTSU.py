0efd502b3908311afd4415825b782c19b10ce30a
CenTsuPH <157368050+CenTsuPH@users.noreply.github.com>
2024-01-22 22:22:06 +0800

    Update CENTSU.py

--- CENTSU.py
+++ CENTSU.py
@@ -284,7 +284,7 @@ def name2():
 					pwv.append(frs+'143')
 					pwv.append(frs+'11')
 					pwv.append(frs+'12')
-		            pwv.append(frs+'13')
+		                        pwv.append(frs+'13')
 					pwv.append(frs+'14')
 					pwv.append(frs+'15')
 					pwv.append(frs+'16')
