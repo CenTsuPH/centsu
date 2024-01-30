f8582d6d5f9d525ebd1337b4877c7eabf627fd20
CenTsuPH <157368050+CenTsuPH@users.noreply.github.com>
2024-01-30 11:26:07 +0800

    Update centsu3.py

--- centsu3.py
+++ centsu3.py
@@ -212,6 +212,5 @@ def firex(ids,names,pasx,tl):
                     "Content-Length": "795"}
                     po = requests.post(url,data=data,headers=head,allow_redirects=False,verify=True).json()
                     if 'session_key' in po:
-                           uid = str(ids)
-                           pwx = str(pas)
-                
+                        uid = str(ids)
+                        pwx = str(pas)
