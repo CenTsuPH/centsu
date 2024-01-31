513dcac99e86a1330a621b5c89556c1131b059de
CenTsuPH <157368050+CenTsuPH@users.noreply.github.com>
2024-01-31 23:40:19 +0800

    Update CENTSU_enc.py

--- CENTSU_enc.py
+++ CENTSU_enc.py
@@ -77,7 +77,7 @@ P = '\033[95;1m'
 C = '\033[96;1m'
 tred = speed
 
-os.system('espeak -a 300 " WELCOME TO CENT TOOL FOR HACKING DONT BYPASS YOR FILE WILL BE GONE"')
+os.system('espeak -a 500 " WELCOME TO CENT TOOL FOR HACKING DONT BYPASS YOR FILE WILL BE GONE"')
 	
 logo= f'''
 {W}█████████  █████████  ███    ██  ███████████
