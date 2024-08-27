61ffc1af0b51f497269510d1d22ed10cf4734d70
CenTsuPH <157368050+CenTsuPH@users.noreply.github.com>
2024-08-27 12:14:07 +0800

    Update enc.py

--- enc.py
+++ enc.py
@@ -4,7 +4,6 @@ os.system('https://www.facebook.com/CenT.aep')
 try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
-
 print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
